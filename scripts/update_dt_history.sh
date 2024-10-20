310c4b1929f92b6618119fe73f692ed9aa6f97b6
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2024-10-20 13:53:13 -0600

    Try using GitHub's actions/checkout for DT

--- scripts/update_dt_history.sh
+++ scripts/update_dt_history.sh
@@ -3,13 +3,15 @@
 # Always run in repo root
 cd "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/..
 
-mkdir -p tmp
+if [ -z "$CI" ]; then
+    mkdir -p tmp
 
-if [ -d tmp/DefinitelyTyped ]; then
-    # Avoid re-cloning if we already have a repo
-    git -C tmp/DefinitelyTyped fetch origin master:master
-else
-    git clone git@github.com:DefinitelyTyped/DefinitelyTyped.git --bare tmp/DefinitelyTyped
+    if [ -d tmp/DefinitelyTyped ]; then
+        # Avoid re-cloning if we already have a repo
+        git -C tmp/DefinitelyTyped fetch origin master:master
+    else
+        git clone git@github.com:DefinitelyTyped/DefinitelyTyped.git --bare tmp/DefinitelyTyped
+    fi
 fi
 
 git -C tmp/DefinitelyTyped log --pretty=format:"%h %cd" --date=unix > data/dt_history.txt
